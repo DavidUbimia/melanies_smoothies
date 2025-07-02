a1006887df40657c8d50450d50f79c5c8c8b4dab
DavidNika <david.hernandez@pfsgroup.es>
2025-07-02 00:52:55 -0600

    Update streamlit_app.py
    se actualiza

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,10 @@ from snowflake.snowpark.functions import col
 import requests
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+
 # st.text(smoothiefroot_response.json())
 
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
+
 
 
 # Write directly to the app
@@ -54,6 +54,11 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
+
+        
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """',' """ + name_on_order + """')"""
