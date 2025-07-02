2d935306746b681ef0d0c916a8784e44501822ec
DavidNika <david.hernandez@pfsgroup.es>
2025-07-02 00:44:35 -0600

    Update streamlit_app.py
    agregamos el método json en response

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import requests
 
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
 # Write directly to the app
